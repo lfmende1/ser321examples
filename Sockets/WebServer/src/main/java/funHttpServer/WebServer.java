ccfa1adc893f5f3580cd23646ad05bc7211904d1
lfmende1 <lfmende1@asu.edu>
2023-02-09 16:27:47 -0700

    Commiting multiply error handling

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -201,12 +201,7 @@ class WebServer {
           Map<String, String> query_pairs = new LinkedHashMap<String, String>();
           // extract path parameters
           query_pairs = splitQuery(request.replace("multiply?", ""));
-
-          // extract required fields from parameters
-          Integer num1 = Integer.parseInt(query_pairs.get("num1"));
-          Integer num2 = Integer.parseInt(query_pairs.get("num2"));
-
-          if (num1 == null || num2 == null){
+          if (query_pairs.get("num1") == null || query_pairs.get("num2") == null){
 
             builder.append("HTTP/1.1 409 Conflict\n");
             builder.append("Content-Type: text/html; charset=utf-8\n");
@@ -216,6 +211,11 @@ class WebServer {
                     "please enter two numbers to multiply");
             throw exc;
           }
+          // extract required fields from parameters
+          Integer num1 = Integer.parseInt(query_pairs.get("num1"));
+          Integer num2 = Integer.parseInt(query_pairs.get("num2"));
+
+
 
 
           // do math
